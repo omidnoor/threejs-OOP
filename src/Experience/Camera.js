ba44a62acb101666dd6ea586dc2b54c159d63396
Omid Noorshams <o.noorshams@gmail.com>
2023-05-20 23:26:16 -0700

    import OrbitControls directly from three

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/orbitcontrols.js";
+// import { OrbitControls } from "three/examples/jsm/controls/orbitcontrols.js";
+import { OrbitControls } from "three";
 
 import Experience from "./Experience.js";
 
