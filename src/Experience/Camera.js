9d19ec0b013ae17bb1a97719f6cb29689f987bd3
Omid Noorshams <o.noorshams@gmail.com>
2023-05-20 23:37:34 -0700

    npm install three-stdlib

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 // import { OrbitControls } from "three/examples/jsm/controls/orbitcontrols.js";
-import { OrbitControls } from "three";
+// import { OrbitControls } from "three";
+import { OrbitControls } from "three-stdlib/controls/OrbitControls";
 
 import Experience from "./Experience.js";
 
