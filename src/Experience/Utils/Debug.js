35c303059ae8d5bb0056df29b3f841e3e0d84605
Omid Noorshams <o.noorshams@gmail.com>
2023-05-20 23:40:11 -0700

    this.active = true;

--- src/Experience/Utils/Debug.js
+++ src/Experience/Utils/Debug.js
@@ -2,7 +2,8 @@ import Gui from "lil-gui";
 
 export default class Debug {
   constructor() {
-    this.active = window.location.hash === "#debug";
+    // this.active = window.location.hash === "#debug";
+    this.active = true;
 
     if (this.active) {
       this.ui = new Gui();
